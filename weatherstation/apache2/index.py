6e811156f17ef1e0e6a0f333827bba4ed9daf0c9
TeKaPol <47486321+TeKaPol@users.noreply.github.com>
2019-06-04 22:00:53 +0200

    Update index.py

--- weatherstation/apache2/index.py
+++ weatherstation/apache2/index.py
@@ -5,7 +5,7 @@ from weatherstation.apache2.settings import titles
 print("Content-Type: text/html")
 print()
 
-for img in title:
+for img in titles:
     print('''<img src="{}.png">'''.format(img))
 
 
